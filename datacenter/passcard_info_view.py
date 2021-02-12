9f095852ffd7dcb8fb3bbaf85adabf943eb637c0
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2021-02-12 19:42:05 +0300

    Завершил урок

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,45 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+import django
+
+
+def format_duration(duration):
+    days, seconds = duration.days, duration.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 60)
+    return f'{hours}:{minutes}:{seconds}'
+
+
+def get_duration(visitor):
+    entered_moscow_time = django.utils.timezone.localtime(visitor.entered_at)
+    if django.utils.timezone.localtime(visitor.leaved_at) is None:
+        moscow_time = django.utils.timezone.localtime()
+        return moscow_time - entered_moscow_time
+    else:
+        leaved_moscow_time = django.utils.timezone.localtime(visitor.leaved_at)
+        return leaved_moscow_time - entered_moscow_time
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            "entered_at": "11-04-2018",
-            "duration": "25:03",
-            "is_strange": False
-        },
-    ]
+    this_passcard_visits = []
+    passcard = Passcard.objects.get(passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
+    for visit in visits:
+        duration = get_duration(visit)
+        days, seconds = duration.days, duration.seconds
+        hours = days * 24 + seconds // 3600
+        if hours >= 2:
+            is_strange = "Да"
+        else:
+            is_strange = "Нет"
+        visit_card = {
+                "entered_at": django.utils.timezone.localtime(visit.entered_at),
+                "duration": format_duration(duration),
+                "is_strange": is_strange
+            }
+        this_passcard_visits.append(visit_card)
     context = {
         "passcard": passcard,
         "this_passcard_visits": this_passcard_visits
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,40 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+import django
+
+
+def get_duration(visitor):
+    moscow_time = django.utils.timezone.localtime()
+    entered_moscow_time = django.utils.timezone.localtime(visitor.entered_at)
+    return moscow_time - entered_moscow_time
+
+
+def format_duration(duration):
+    days, seconds = duration.days, duration.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 60)
+    return f'{hours}:{minutes}:{seconds}'
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    non_closed_visits = []
+    visitors = Visit.objects.filter(leaved_at=None)
+    for visitor in visitors:
+        duration = get_duration(visitor)
+        days, seconds = duration.days, duration.seconds
+        hours = days * 24 + seconds // 3600
+        if hours >= 2:
+            is_strange = "Да"
+        else:
+            is_strange = "Нет"
+        visitor_data = {"who_entered": visitor.passcard,
+                        "entered_at": django.utils.timezone.localtime(visitor.entered_at),
+                        "duration": format_duration(duration),
+                        "is_strange": is_strange}
+    non_closed_visits.append(visitor_data)
 
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
-        }
-    ]
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
